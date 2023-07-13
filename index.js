8ffcc503d625409a27ebd506a3cf9fdfde35803e
Sergey Vorobyev <syvorobyev@gmail.com>
2023-07-13 21:31:20 +0400

    add index

--- index.js
+++ index.js
@@ -3,29 +3,33 @@ import SmoothScroll from "smoothscroll-for-websites";
 import { useCursor } from "./src/cursor/script";
 import './src/common.css';
 
-useZomingPhotos();
-useCursor();
-SmoothScroll({
-    // Scrolling Core
-    animationTime    : 800, // [ms]
-    stepSize         : 100, // [px]
+if (!window.isCustomCodeInitialized) {
+    useZomingPhotos();
+    useCursor();
+    SmoothScroll({
+        // Scrolling Core
+        animationTime    : 800, // [ms]
+        stepSize         : 100, // [px]
+    
+        // Acceleration
+        accelerationDelta : 5,  // 50
+        accelerationMax   : 3,   // 3
+    
+        // Keyboard Settings
+        keyboardSupport   : true,  // option
+        arrowScroll       : 300,    // [px]
+    
+        // Pulse (less tweakable)
+        // ratio of "tail" to "acceleration"
+        pulseAlgorithm   : true,
+        pulseScale       : 4,
+        pulseNormalize   : 1,
+    
+        // Other
+        touchpadSupport   : false, // ignore touchpad by default
+        fixedBackground   : false, 
+        excluded          : '' 
+    });
 
-    // Acceleration
-    accelerationDelta : 5,  // 50
-    accelerationMax   : 3,   // 3
-
-    // Keyboard Settings
-    keyboardSupport   : true,  // option
-    arrowScroll       : 300,    // [px]
-
-    // Pulse (less tweakable)
-    // ratio of "tail" to "acceleration"
-    pulseAlgorithm   : true,
-    pulseScale       : 4,
-    pulseNormalize   : 1,
-
-    // Other
-    touchpadSupport   : false, // ignore touchpad by default
-    fixedBackground   : false, 
-    excluded          : '' 
-})
+    window.isCustomCodeInitialized = true;
+}
