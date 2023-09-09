0012149cdc535f82b2ce4438179b35d2e324c43a
Sergey Vorobyev <syvorobyev@gmail.com>
2023-09-09 19:21:30 +0400

    fade photos on hover

--- darStudioRender.js
+++ darStudioRender.js
@@ -4,6 +4,7 @@ import { usePortfolioSwitch, DAR_CATEGORIES } from "./src/portfolioSwitch/script
 import { usePortfolioZoom } from "./src/portfolioZoom/script";
 
 import './src/darStudioRender/style.scss';
+import './src/portfolioDoubleImages/style.scss';
 
 usePortfolioSwitch(DAR_CATEGORIES);
 usePortfolioZoom();
