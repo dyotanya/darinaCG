73a79dc63f504c732f1baf9c4b3d6400afea3e43
Sergey Vorobyev <syvorobyev@gmail.com>
2023-09-30 19:04:09 +0400

    add about me flying photos

--- entries/about.js
+++ entries/about.js
@@ -1,7 +1,9 @@
 import { useTags } from "../src/about/tags/script";
 import { useRunningLine } from "../src/runningLine/script";
+import { useFlyingPhotos } from "../src/about/flyingPhotos/script";
 
 import '../src/styles/common.scss';
 
 useTags();
 useRunningLine();
+useFlyingPhotos();
---
+++ src/about/flyingPhotos/script.js
@@ -0,0 +1,32 @@
+import './style.scss';
+
+export function useFlyingPhotos() {
+    const groups = document.querySelectorAll('.flyingphotosgroup');
+
+    groups.forEach((group) => {
+        const section = group.dataset.section;
+        const trigger = document.querySelector(`.workareastag[data-section="${section}"]`);
+        if (!trigger) {
+            return console.error(`No trigger for ${section} flyting photos`);
+        }
+        trigger.addEventListener('mouseenter', () => {
+            showSection(group);
+        });
+        trigger.addEventListener('mouseleave', () => {
+            hideSection(group);
+        });
+
+        const photos = group.querySelectorAll('.flyingphoto');
+        photos.forEach((photo, index) => {
+            photo.style.setProperty('--delay', `${index * 0.1}s`);
+        });
+    });
+
+    function showSection(group) {
+        group.classList.add('shown');
+    }
+
+    function hideSection(group) {
+        group.classList.remove('shown');
+    }
+};
